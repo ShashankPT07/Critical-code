ac8fa5be8fea32e05d19f88dc006b0ea00fe1844
ShashankPT07 <shashankpt.xworks@gamil.com>
2025-09-09 16:02:15 +0530

    Chaining

---
+++ chain method/CartoonRunner.java
@@ -0,0 +1,7 @@
+class CartoonRunner {
+    public static void main(String[] args) {
+        Cartoon cartoon = new Cartoon();
+        cartoon.characterName("Tom");
+        System.out.println("Actual object value: " + cartoon);
+    }
+}
---
+++ chain method/HockeyRunner.java
@@ -0,0 +1,8 @@
+class HockeyRunner {
+    public static void main(String[] args) {
+
+        Hockey hockey = new Hockey();
+        hockey.playerName("maga");
+        System.out.println("actual value: " + hockey);
+    }
+}
