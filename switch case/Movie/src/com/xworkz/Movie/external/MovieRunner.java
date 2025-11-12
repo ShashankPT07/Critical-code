96fb18033cf987b3d0ee3dc0b5110e968ec9f4ae
ShashankPT07 <shashankpt.xworks@gamil.com>
2025-11-12 15:33:09 +0530

    switch

---
+++ switch case/Movie/src/com/xworkz/Movie/external/MovieRunner.java
@@ -0,0 +1,10 @@
+package com.xworkz.Movie.external;
+
+import com.xworkz.Movie.internal.Movie;
+
+public class MovieRunner {
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.budget(1);
+    }
+}
---
+++ switch case/Movie/src/com/xworkz/Movie/internal/Movie.java
@@ -0,0 +1,20 @@
+package com.xworkz.Movie.internal;
+
+public class Movie {
+    public  void budget(int price){
+
+        switch (price) {
+
+
+            case 1:
+                System.out.println("eega");
+                break;
+            case 2:
+                System.out.println("eeg");
+                break;
+            default:
+                System.out.println("invalid ");
+
+        }
+    }
+}
